9e00f2037a9a9d0bfb80e8c2ae0202d7eac93e8f
Dimitry <dimitry@ethereum.org>
2018-03-21 17:12:15 +0300

    set default gasPrice to 0

--- eth/main.cpp
+++ eth/main.cpp
@@ -1062,7 +1062,6 @@ int main(int argc, char** argv)
 
     web3.setIdealPeerCount(peers);
     web3.setPeerStretch(peerStretch);
-//  std::shared_ptr<eth::BasicGasPricer> gasPricer = make_shared<eth::BasicGasPricer>(u256(double(ether / 1000) / etherPrice), u256(blockFees * 1000));
     std::shared_ptr<eth::TrivialGasPricer> gasPricer = make_shared<eth::TrivialGasPricer>(askPrice, bidPrice);
     eth::Client* c = nodeMode == NodeMode::Full ? web3.ethereum() : nullptr;
     if (c)
